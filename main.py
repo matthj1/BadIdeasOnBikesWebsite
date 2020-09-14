5b73f1eeae2db1b5e76ebda7bf957eb46e16c9f3
Joe Matthews <joematthewsphotography@gmail.com>
2020-09-14 17:55:42 +0100

    learning more about template inheritence

--- main.py
+++ main.py
@@ -2,16 +2,29 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+posts = ({
+    "author": "Joe Matthews",
+    "title": "Kent Climbs",
+    "content": "This is a blog post about bike climbs in kent",
+    "date": "14/09/2020"
+},
+    {
+    "author": "Joe Matthews",
+    "title": "Rutland Circuit",
+    "content": "This is a blog post about a bike ride in rutland",
+    "date": "13/09/2020"
+
+})
 
 @app.route("/")
 @app.route("/home")
 def homePage():
-    return render_template("BASE.html", stuff="No")
+    return render_template("Home.html", posts=posts)
 
 
 @app.route("/about")
 def aboutPage():
-    return "<h1>About Page</h1>"
+    return render_template("About.html")
 
 
 if __name__ == "__main__":
