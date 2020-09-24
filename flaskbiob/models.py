0cc158f24b6e05747d9cca3acd188cc67126079b
Joe Matthews <joematthewsphotography@gmail.com>
2020-09-24 16:46:05 +0100

    more work on database communication

--- flaskbiob/models.py
+++ flaskbiob/models.py
@@ -1,8 +1,12 @@
-from flaskbiob import db
+from flaskbiob import db, login_manager
 from datetime import datetime
+from flask_login import UserMixin
 
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
 
-class Users(db.Model):
+class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
